63fbdcfdd4166d0988746a8b88de4ab3ed1df837
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-05 10:54:59 +0100

    Created Address entity

---
+++ Ecommerce.API/Entities/Address.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ecommerce.API.Entities;
+
+public class Address : BaseEntity
+{
+    public string AddressLine { get; set; }
+    public string ZipCode { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+
+    [ForeignKey("CountryId")]
+    public Country Country { get; set; }
+    public string CountryId { get; set; }
+}
