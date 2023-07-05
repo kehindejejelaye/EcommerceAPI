612b33722847cab0bd35d0becc64730c49898a45
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-05 11:34:58 +0100

    Added List of VariantOption to Variant entity

--- Ecommerce.API/Entities/Variant.cs
+++ Ecommerce.API/Entities/Variant.cs
@@ -9,4 +9,5 @@ public class Variant : BaseEntity
     [ForeignKey("CategoryId")]
     public Category Category { get; set; }
     public string CategoryId { get; set; }
+    public List<VariantOption> VariantOptions { get; set; }
 }
