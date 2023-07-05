397054481ee0e17a1c7ec2ad2ccee23319c4b7a1
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-05 12:47:18 +0100

    Created and populated context class with the necessary entities

---
+++ Ecommerce.API/Data/EcommerceContext.cs
@@ -0,0 +1,29 @@
+﻿using Ecommerce.API.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.API.Data;
+
+public class EcommerceContext : IdentityDbContext<ApplicationUser>
+{
+    public EcommerceContext(DbContextOptions<EcommerceContext> options) : base(options) { }
+
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<Favorite> Favorites { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<OrderStatus> OrderStatus { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductItem> ProductItems { get; set; }
+    public DbSet<ProductItemVariantOption> ProductItemVariantOptions { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+    public DbSet<Variant> Variants { get; set; }
+    public DbSet<VariantOption> VariantOptions { get; set; }
+    public DbSet<WishList> WishLists { get; set; }
+
+
+
+}
