242983c166f9f22748ef3bad6730243a15875f8c
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-05 12:04:13 +0100

    Added OrderStatus class

---
+++ Ecommerce.API/Entities/EntityConfiguration/OrderStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.API.Entities.EntityConfiguration;
+
+public class OrderStatus : BaseEntity
+{
+    public string Status { get; set; }
+
+    public ICollection<Order> Orders { get; set; }
+}
