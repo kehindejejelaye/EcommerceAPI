7fff9d758c7bf9a7bc5dd7fd773b07ccdae8857c
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-05 11:14:26 +0100

    Created ProductItem entity

---
+++ Ecommerce.API/Entities/ProductItem.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ecommerce.API.Entities;
+
+public class ProductItem : BaseEntity
+{
+    public string SKU { get; set; }
+    public string QuantityInStock { get; set; }
+    public string ImageUrl { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+
+    [ForeignKey("ProductId")]
+    public Product Product { get; set; }
+    public string ProductId { get; set; }
+}
