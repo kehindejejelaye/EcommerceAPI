89b40210e798cbe974de4446dc32fe64b54721bc
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-05 11:11:22 +0100

    Created Product entity

---
+++ Ecommerce.API/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ecommerce.API.Entities;
+
+public class Product : BaseEntity 
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; } = string.Empty;
+
+    [ForeignKey("CategoryId")]
+    public Category Category { get; set; }
+    public string CategoryId { get; set; }
+}
