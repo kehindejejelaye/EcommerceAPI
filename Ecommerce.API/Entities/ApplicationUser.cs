64471b0964e8efa3d38e5bc1b5487c7564142cdf
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-05 10:42:08 +0100

    Added ApplicationUser entity

---
+++ Ecommerce.API/Entities/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Net;
+
+namespace Ecommerce.API.Entities;
+
+public class ApplicationUser : IdentityUser
+{
+    public ICollection<Order> Orders { get; set; }
+    public ICollection<Address> Addresses { get; set; }
+
+    public ICollection<Favorite> Favorites { get; set; }
+
+    public ICollection<Review> Reviews { get; set; }
+
+    public ICollection<ShoppingCartItem> ShoppingCartItems { get; set; }
+    public ICollection<WishList> WishList { get; set; }
+}
