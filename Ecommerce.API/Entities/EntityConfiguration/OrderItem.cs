a4d1c949bb7fed2f63a3ca54e44bea9e4084c02e
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-05 12:11:52 +0100

    Added OrderItem class

---
+++ Ecommerce.API/Entities/EntityConfiguration/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ecommerce.API.Entities.EntityConfiguration;
+
+public class OrderItem : BaseEntity
+{
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+
+    [ForeignKey("ProductItemId")]
+    public string ProductItemId { get; set; }
+    public ProductItem ProductItem { get; set; }
+
+    [ForeignKey("OrderId")]
+    public string OrderId { get; set; }
+    public Order Order { get; set; }
+}
