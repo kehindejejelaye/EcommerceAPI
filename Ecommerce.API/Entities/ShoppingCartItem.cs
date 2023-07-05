65b7b24b58aa2c4cd4e07b3fc9bfe82c76669d2d
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-05 12:38:15 +0100

    Created ShoppingCartItem class

---
+++ Ecommerce.API/Entities/ShoppingCartItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ecommerce.API.Entities;
+
+public class ShoppingCartItem : BaseEntity
+{
+    public int Quantity { get; set; } = 1;
+
+    [ForeignKey("ProductItemId")]
+    public string ProductItemId { get; set; }
+    public ProductItem ProductItem { get; set; }
+
+    [ForeignKey("UserId")]
+    public string UserId { get; set; }
+    public ApplicationUser User { get; set; }
+}
+
