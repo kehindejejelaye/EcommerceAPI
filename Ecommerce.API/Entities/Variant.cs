71a9d85114126b724d35871e1eb236f7f39e8f0f
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-05 11:06:02 +0100

    Created Variant entity

---
+++ Ecommerce.API/Entities/Variant.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ecommerce.API.Entities;
+
+public class Variant : BaseEntity
+{
+    public string Name { get; set; }
+
+    [ForeignKey("CategoryId")]
+    public Category Category { get; set; }
+    public string CategoryId { get; set; }
+}
