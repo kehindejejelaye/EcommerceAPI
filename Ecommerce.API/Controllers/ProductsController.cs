44603094ccb9268f14f1963e9cb077cba86e64c3
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-06 13:40:33 +0100

    Setup ProductsController.cs and injected dependencies via constructor

---
+++ Ecommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Ecommerce.API.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers;
+
+[Route("api/products")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly IRepositoryManager _repoManager;
+    private readonly IMapper _mapper;
+
+    public ProductsController(IRepositoryManager repoManager, IMapper mapper)
+    {
+        _repoManager = repoManager;
+        _mapper = mapper;
+    }
+}
