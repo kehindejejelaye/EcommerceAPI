58ee3550b4382fc7ab4f3b39845f7dc3bd2b4d7c
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-06 13:30:16 +0100

    Add IProductRepository.cs

---
+++ Ecommerce.API/Contracts/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Ecommerce.API.Entities;
+
+namespace Ecommerce.API.Contracts;
+
+public interface IProductRepository
+{
+    void CreateProduct(Product product);
+    void DeleteProduct(Product product);
+    Task<IEnumerable<Product>> GetAllProducts(bool trackChanges);
+    Task<Product?> GetProductById(string categoryId, string productId, bool trackChanges);
+    void UpdateProduct(Product product);
+}
