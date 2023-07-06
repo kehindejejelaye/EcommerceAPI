b21c2f58faf99bbacf8a190281ccbebac0921742
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-06 11:19:37 +0100

    Created Mapping profiles for Category

---
+++ Ecommerce.API/MappingProfiles/CategoryProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Ecommerce.API.DTOs.Category;
+using Ecommerce.API.Entities;
+
+namespace Ecommerce.API.MappingProfiles;
+
+public class CategoryProfile : Profile
+{
+    public CategoryProfile()
+    {
+        CreateMap<CreateCategoryDto, CreateCategoryDto>();
+        CreateMap<UpdateCategoryDto, CreateCategoryDto>();
+        CreateMap<Category, ReadCategoryDto>();
+    }
+}
