78e4f5372cf5c88cb2aa884665b293364984f0a3
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-06 13:05:25 +0100

    Set up CategoriesController and injected dependencies

---
+++ Ecommerce.API/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Ecommerce.API.Contracts;
+using Ecommerce.API.DTOs.Category;
+using Ecommerce.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers;
+
+[Route("api/categories")]
+[ApiController]
+public class CategoriesController : ControllerBase
+{
+    private readonly IRepositoryManager _repoManager;
+    private readonly IMapper _mapper;
+
+    public CategoriesController(IRepositoryManager repoManager, IMapper mapper)
+    {
+        _repoManager = repoManager;
+        _mapper = mapper;
+    }
+
+    
+}
