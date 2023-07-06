9cd703e65a57dd36f3696b0c08ab7f466e4fecc2
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-06 14:13:47 +0100

    Added ProductDtos for Create, Read, and Update

---
+++ Ecommerce.API/DTOs/Product/CreateProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.API.DTOs.Product;
+
+public class CreateProductDto
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public string CategoryId { get; set; }
+}
---
+++ Ecommerce.API/DTOs/Product/ReadProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Ecommerce.API.DTOs.Product;
+
+public class ReadProductDto
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public string CategoryId { get; set; }
+}
