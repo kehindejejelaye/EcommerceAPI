ead71ca68a8ee3c803a94fe51d06fadf5b4a9b3c
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-06 14:20:07 +0100

    Added GetAllProductsInACategory action

--- Ecommerce.API/Controllers/ProductsController.cs
+++ Ecommerce.API/Controllers/ProductsController.cs
@@ -1,10 +1,11 @@
 ﻿using AutoMapper;
 using Ecommerce.API.Contracts;
+using Ecommerce.API.DTOs.Product;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerce.API.Controllers;
 
-[Route("api/products")]
+[Route("api/categories/{categoryId}/products")]
 [ApiController]
 public class ProductsController : ControllerBase
 {
@@ -15,5 +16,13 @@ public class ProductsController : ControllerBase
     {
         _repoManager = repoManager;
         _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> GetAllProductsInACategory(string categoryId)
+    {
+        var products = await _repoManager.Product.GetAllProductsInACategory(categoryId, false);
+        var _products = _mapper.Map<IEnumerable<ReadProductDto>>(products);
+        return Ok(_products);
     }
 }
