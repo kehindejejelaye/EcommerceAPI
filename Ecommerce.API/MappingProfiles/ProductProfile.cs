1fe9439e68b6ff585f13e23652228ad91a29acf8
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-06 14:12:55 +0100

    Adding mapping profiles for ProductDtos

---
+++ Ecommerce.API/MappingProfiles/ProductProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Ecommerce.API.DTOs.Product;
+using Ecommerce.API.Entities;
+
+namespace Ecommerce.API.MappingProfiles;
+
+public class ProductProfile : Profile
+{
+    public ProductProfile()
+    {
+        CreateMap<Product, ReadProductDto>();
+        CreateMap<CreateProductDto, Product>();
+        CreateMap<UpdateProductDto, Product>();
+    }
+}
