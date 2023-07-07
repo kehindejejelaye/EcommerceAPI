277ab0ad06424a2c8ff975220258cd6c33a20faa
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-07 09:14:42 +0100

    Created a shoppingCartItemRepository and injected the database context into its constructor

---
+++ Ecommerce.API/Repositories/ShoppingCartItemRepository.cs
@@ -0,0 +1,11 @@
+﻿using Ecommerce.API.Data;
+using Ecommerce.API.Entities;
+
+namespace Ecommerce.API.Repositories;
+
+public class ShoppingCartItemRepository : BaseRepository<ShoppingCartItem>
+{
+    public ShoppingCartItemRepository(EcommerceContext _ecomContext) : base(_ecomContext)
+    {
+    }
+}
