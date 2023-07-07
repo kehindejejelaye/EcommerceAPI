f9d2544ff8f6e641736b914351516a150f24c725
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-07 09:25:12 +0100

    Created AddToCartI()

--- Ecommerce.API/Repositories/ShoppingCartItemRepository.cs
+++ Ecommerce.API/Repositories/ShoppingCartItemRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Ecommerce.API.Data;
 using Ecommerce.API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce.API.Repositories;
 
@@ -7,5 +8,18 @@ public class ShoppingCartItemRepository : BaseRepository<ShoppingCartItem>
 {
     public ShoppingCartItemRepository(EcommerceContext _ecomContext) : base(_ecomContext)
     {
+    }
+
+    public async void AddToCart(ShoppingCartItem item)
+    {
+        var shoppingCartItem = await FindByCondition(_item => item.UserId == _item.UserId && item.ProductItemId == _item.ProductItemId, trackChanges: false).SingleOrDefaultAsync();
+
+        if (shoppingCartItem == null)
+        {
+            Create(item);
+            return;
+        }
+
+        shoppingCartItem.Quantity += item.Quantity;
     }
 }
