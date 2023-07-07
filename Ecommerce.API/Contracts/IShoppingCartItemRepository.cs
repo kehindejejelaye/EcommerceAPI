7c83fa33d0831cf7b9e8de02a7ba63c5da859fde
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-07 09:43:30 +0100

    Added IShoppingCartItemRepository.cs

---
+++ Ecommerce.API/Contracts/IShoppingCartItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.API.Entities;
+
+namespace Ecommerce.API.Contracts
+{
+    public interface IShoppingCartItemRepository
+    {
+        void AddToCart(ShoppingCartItem item);
+        void ClearShoppingCart(string userId);
+        List<ShoppingCartItem> GetShoppingCartItems(string userId);
+        decimal GetShoppingCartTotal(string userId);
+        void RemoveFromCart(ShoppingCartItem item);
+    }
+}
