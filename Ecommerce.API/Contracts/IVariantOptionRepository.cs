63c6e93aeed6266121d667c12daa9315865690a3
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-07 03:14:24 +0100

    Created VariantOptionRepository and it's interface

---
+++ Ecommerce.API/Contracts/IVariantOptionRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.API.Entities;
+
+namespace Ecommerce.API.Contracts
+{
+    public interface IVariantOptionRepository
+    {
+        void CreateVariantOption(VariantOption variantOption);
+        void DeleteVariantOption(VariantOption variantOption);
+        Task<IEnumerable<VariantOption>> GetAllVariantOptionsInVariant(string variantId, bool trackChanges);
+        Task<VariantOption?> GetVariantOptionById(string variantId, string variantOptionId, bool trackChanges);
+        void UpdateVariantOption(VariantOption variantOption);
+    }
+}
---
+++ Ecommerce.API/Repositories/VariantOptionRepository.cs
@@ -0,0 +1,33 @@
+﻿using Ecommerce.API.Contracts;
+using Ecommerce.API.Data;
+using Ecommerce.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.API.Repositories;
+
+public class VariantOptionRepository : BaseRepository<VariantOption>, IVariantOptionRepository
+{
+    public VariantOptionRepository(EcommerceContext _ecomContext) : base(_ecomContext)
+    {
+    }
+
+    public async Task<VariantOption?> GetVariantOptionById(string variantId, string variantOptionId, bool trackChanges)
+    {
+        return await FindByCondition(vo => vo.Id == variantOptionId && vo.VariantId == variantId, trackChanges)
+            .Include(vo => vo.Variant)
+            .SingleOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<VariantOption>> GetAllVariantOptionsInVariant(string variantId, bool trackChanges)
+    {
+        return await FindByCondition(vo => vo.VariantId == variantId, trackChanges)
+            .OrderBy(vo => vo.Value)
+            .ToListAsync();
+    }
+
+    public void CreateVariantOption(VariantOption variantOption) => Create(variantOption);
+
+    public void DeleteVariantOption(VariantOption variantOption) => Delete(variantOption);
+
+    public void UpdateVariantOption(VariantOption variantOption) => Update(variantOption);
+}
