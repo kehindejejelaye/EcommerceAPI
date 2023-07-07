3add15db0ba187362a19e89d86918da9a7a46eaa
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-07 02:00:41 +0100

    Created IVariantRepository.cs

---
+++ Ecommerce.API/Contracts/IVariantRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.API.Entities;
+
+namespace Ecommerce.API.Contracts
+{
+    public interface IVariantRepository
+    {
+        void CreateVariant(Variant variant);
+        void DeleteVariant(Variant variant);
+        Task<IEnumerable<Variant>> GetAllVariantsInACategory(string categoryId, bool trackChanges);
+        Task<Variant?> GetVariantById(string categoryId, string variantId, bool trackChanges);
+        void UpdateVariant(Variant variant);
+    }
+}
