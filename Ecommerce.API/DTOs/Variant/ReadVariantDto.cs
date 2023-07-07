4a74225a54bbd9d1db1d785aeb39d84a6a43a172
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-07 02:12:29 +0100

    Added ReadVariantDto

---
+++ Ecommerce.API/DTOs/Variant/ReadVariantDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.API.DTOs.Variant;
+
+public class ReadVariantDto
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string CategoryId { get; set; }
+}
