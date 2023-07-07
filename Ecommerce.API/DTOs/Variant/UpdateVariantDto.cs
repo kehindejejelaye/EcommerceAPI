743da32a59accf99369fb34f192b18b0da973bde
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-07 02:12:41 +0100

    Added UpdateVariantDto

---
+++ Ecommerce.API/DTOs/Variant/UpdateVariantDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.API.DTOs.Variant;
+
+public class UpdateVariantDto
+{
+    public string Name { get; set; }
+    public string CategoryId { get; set; }
+    private DateTime UpdatedAt { get;  set; } = DateTime.Now;
+}
