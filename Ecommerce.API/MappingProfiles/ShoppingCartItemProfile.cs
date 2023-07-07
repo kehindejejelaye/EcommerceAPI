1f611e44f3a028921827ec59d7610e10d1ebe170
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-07 10:43:18 +0100

    Added mapping for ShoppingCartItem

---
+++ Ecommerce.API/MappingProfiles/ShoppingCartItemProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Ecommerce.API.DTOs.ShoppingCartItem;
+using Ecommerce.API.Entities;
+
+namespace Ecommerce.API.MappingProfiles;
+
+public class ShoppingCartItemProfile : Profile
+{
+    public ShoppingCartItemProfile()
+    {
+        CreateMap<CreateShoppingCartItemDto, ShoppingCartItem>();
+        CreateMap<UpdateShoppingCartItemDto, ShoppingCartItem>();
+        CreateMap<ShoppingCartItem, ReadShoppingCartItemDto>();
+    }
+}
