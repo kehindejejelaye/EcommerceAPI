98077ef2176969dd3949244dc10a6547f949aee1
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-07 02:49:29 +0100

    Added mapping profiles for ProductItem

---
+++ Ecommerce.API/MappingProfiles/ProductItemProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Ecommerce.API.DTOs.ProductItem;
+using Ecommerce.API.Entities;
+using Ecommerce.API.Repositories;
+
+namespace Ecommerce.API.MappingProfiles;
+
+public class ProductItemProfile : Profile
+{
+	public ProductItemProfile()
+	{
+		CreateMap<ProductItem, ReadProductItemDto>();
+		CreateMap<CreateProductItemDto, ProductItem>();
+		CreateMap<UpdateProductItemDto, ProductItem>();
+	}
+}
