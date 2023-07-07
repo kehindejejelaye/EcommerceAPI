da472e614f221ff897f25822f91d27c6711e775c
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-07 03:26:32 +0100

    Created mapping for VariantOption

---
+++ Ecommerce.API/MappingProfiles/VariantOptionProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Ecommerce.API.DTOs.VariantOption;
+using Ecommerce.API.Entities;
+
+namespace Ecommerce.API.MappingProfiles;
+
+public class VariantOptionProfile : Profile
+{
+    public VariantOptionProfile()
+    {
+        CreateMap<VariantOption, ReadVariantOptionDto>();
+        CreateMap<CreateVariantOptionDto, VariantOption>();
+        CreateMap<UpdateVariantOptionDto, VariantOption>();
+    }
+}
