a6b3c167274aaab8b91a27c47f8254429014b958
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-07 02:14:46 +0100

    Added Variant mapping to VariantProfile.cs

---
+++ Ecommerce.API/MappingProfiles/VariantProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Ecommerce.API.DTOs.Variant;
+using Ecommerce.API.Entities;
+
+namespace Ecommerce.API.MappingProfiles;
+
+public class VariantProfile : Profile
+{
+    public VariantProfile()
+    {
+        CreateMap<Variant, ReadVariantDto>();
+        CreateMap<CreateVaraintDto, Variant>();
+        CreateMap<UpdateVariantDto, Variant>();
+    }
+}
