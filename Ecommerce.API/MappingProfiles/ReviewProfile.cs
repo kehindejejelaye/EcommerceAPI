c96b62b11b8a0308312d5ef21cf121795cc82c2a
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-08 22:20:16 +0100

    Added mapping profile for Review entity and dtos

---
+++ Ecommerce.API/MappingProfiles/ReviewProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Ecommerce.API.DTOs.Review;
+using Ecommerce.API.Entities;
+
+namespace Ecommerce.API.MappingProfiles;
+
+public class ReviewProfile : Profile
+{
+    public ReviewProfile()
+    {
+        CreateMap<Review, ReadReviewDto>();
+        CreateMap<CreateReviewDto, Review>();
+        CreateMap<UpdateReviewDto, Review>();
+    }
+}
