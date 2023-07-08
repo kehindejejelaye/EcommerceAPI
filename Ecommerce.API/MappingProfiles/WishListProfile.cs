1a9f315a68cafb28b0d39a21383ccb67c7613850
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-08 14:46:17 +0100

    Adding mapping for wishList

---
+++ Ecommerce.API/MappingProfiles/WishListProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Ecommerce.API.DTOs.WishList;
+using Ecommerce.API.Entities;
+
+namespace Ecommerce.API.MappingProfiles;
+
+public class WishListProfile : Profile
+{
+    public WishListProfile()
+    {
+        CreateMap<WishList, ReadWishListDto>();
+        CreateMap<CreateWishListDto, WishList>();
+        CreateMap<UpdateWishListDto, WishList>();
+    }
+}
