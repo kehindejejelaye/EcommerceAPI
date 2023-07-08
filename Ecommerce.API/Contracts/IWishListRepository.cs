2916925bc3e1ee0ee64d30d795ce354c2b792e84
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-08 14:37:01 +0100

    Added WishListRepository and corresponding interface

---
+++ Ecommerce.API/Contracts/IWishListRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.API.Entities;
+
+namespace Ecommerce.API.Contracts
+{
+    public interface IWishListRepository
+    {
+        void AddWishList(WishList wishList);
+        void DeleteWishList(WishList wishList);
+        Task<WishList?> GetWishListForUserById(string userId, string wishListId, bool trackChanges);
+        Task<IEnumerable<WishList>> GetWishListsForUserByUserId(string userId, bool trackChanges);
+        void UpdateWishList(WishList wishList);
+    }
+}
---
+++ Ecommerce.API/Repositories/WishListRepository.cs
@@ -0,0 +1,33 @@
+﻿using Ecommerce.API.Contracts;
+using Ecommerce.API.Data;
+using Ecommerce.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.API.Repositories;
+
+public class WishListRepository : BaseRepository<WishList>, IWishListRepository
+{
+    public WishListRepository(EcommerceContext ecommerceContext) : base(ecommerceContext)
+    {
+    }
+
+    public async Task<WishList?> GetWishListForUserById(string userId, string wishListId, bool trackChanges)
+    {
+        return await FindByCondition(w => w.Id == wishListId && w.UserId == userId, trackChanges)
+            .Include(w => w.ProductItem)
+            .SingleOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<WishList>> GetWishListsForUserByUserId(string userId, bool trackChanges)
+    {
+        return await FindByCondition(w => w.UserId == userId, trackChanges)
+            .Include(w => w.ProductItem)
+            .ToListAsync();
+    }
+
+    public void AddWishList(WishList wishList) => Create(wishList);
+
+    public void UpdateWishList(WishList wishList) => Update(wishList);
+
+    public void DeleteWishList(WishList wishList) => Delete(wishList);
+}
