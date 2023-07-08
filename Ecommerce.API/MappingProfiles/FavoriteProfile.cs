1c7dcf8b6a02a79842303a6a8d946670b8a1d0bc
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-08 14:09:38 +0100

    Created mapping profile for Favorite

---
+++ Ecommerce.API/MappingProfiles/FavoriteProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Ecommerce.API.DTOs.Favorite;
+using Ecommerce.API.Entities;
+
+namespace Ecommerce.API.MappingProfiles;
+
+public class FavoriteProfile : Profile
+{
+    public FavoriteProfile()
+    {
+        CreateMap<Favorite, ReadFavoriteDto>();
+        CreateMap<CreateFavoriteDto, Favorite>();
+        CreateMap<UpdateFavoriteDto, Favorite>();
+    }
+}
