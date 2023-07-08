9b55332881791c39b25709bfb72f91088fcafaa6
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-08 13:19:48 +0100

    Added mapping profiles for address entity

---
+++ Ecommerce.API/MappingProfiles/AddressProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Ecommerce.API.DTOs.Address;
+using Ecommerce.API.Entities;
+
+namespace Ecommerce.API.MappingProfiles;
+
+public class AddressProfile : Profile
+{
+    public AddressProfile()
+    {
+        CreateMap<CreateAddressDto, Address>();
+        CreateMap<UpdateAddressDto, Address>();
+        CreateMap<Address, ReadAddressDto>();
+    }
+}
