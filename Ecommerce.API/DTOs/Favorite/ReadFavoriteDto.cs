319121f5052436b859360f6ad60f871aeab8f93e
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-08 14:07:06 +0100

    Added Dtos for Favorite entity

---
+++ Ecommerce.API/DTOs/Favorite/ReadFavoriteDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.API.DTOs.Favorite;
+
+public class ReadFavoriteDto
+{
+    public string Id { get; set; }
+    public string ProductItemId { get; set; }
+    public string UserId { get; set; }
+}
---
+++ Ecommerce.API/DTOs/Favorite/UpdateFavoriteDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Ecommerce.API.DTOs.Favorite;
+
+public class UpdateFavoriteDto
+{
+    public string ProductItemId { get; set; }
+    public string UserId { get; set; }
+}
