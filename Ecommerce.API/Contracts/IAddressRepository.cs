1496980e8ad933b87ba149454808a36ddfe23588
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-08 12:58:59 +0100

    Added AddressRepository and corresponding interface

---
+++ Ecommerce.API/Contracts/IAddressRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.API.Entities;
+
+namespace Ecommerce.API.Contracts
+{
+    public interface IAddressRepository
+    {
+        void AddAddress(Address address);
+        void DeleteAddress(Address address);
+        Task<IEnumerable<Address>> GetAddressesForUserByUserId(string userId);
+        Task<Address?> GetAddressForUserById(string userId, string addressId);
+        void UpdateAddress(Address address);
+    }
+}
---
+++ Ecommerce.API/Repositories/AddressRepository.cs
@@ -0,0 +1,32 @@
+﻿using Ecommerce.API.Contracts;
+using Ecommerce.API.Data;
+using Ecommerce.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.API.Repositories;
+
+public class AddressRepository : BaseRepository<Address>, IAddressRepository
+{
+    public AddressRepository(EcommerceContext ecommerceContext) : base(ecommerceContext)
+    {
+    }
+
+    public async Task<Address?> GetAddressForUserById(string userId, string addressId)
+    {
+        return await FindByCondition(a => a.Id == addressId && a.UserId == userId, trackChanges: false)
+            .SingleOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<Address>> GetAddressesForUserByUserId(string userId)
+    {
+        return await FindByCondition(a => a.UserId == userId, trackChanges: false)
+            .Include(a => a.Country)
+            .ToListAsync();
+    }
+
+    public void AddAddress(Address address) => Create(address);
+
+    public void UpdateAddress(Address address) => Update(address);
+
+    public void DeleteAddress(Address address) => Delete(address);
+}
