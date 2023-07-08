cf2cc5c00a28a9844412163729e9cd3de12e121c
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-08 14:02:10 +0100

    Added FavoriteRepository and corresponding contract

---
+++ Ecommerce.API/Contracts/IFavoriteRepository.cs
@@ -0,0 +1,12 @@
+﻿using Ecommerce.API.Entities;
+
+namespace Ecommerce.API.Contracts;
+
+public interface IFavoriteRepository
+{
+    void AddFavorite(Favorite favorite);
+    void DeleteFavorite(Favorite favorite);
+    Task<Favorite> GetFavoriteById(string favoriteId, bool trackChanges);
+    Task<IEnumerable<Favorite>> GetFavoritesByUserId(string userId, bool trackChanges);
+    void UpdateFavorite(Favorite favorite);
+}
---
+++ Ecommerce.API/Repositories/FavoriteRepository.cs
@@ -0,0 +1,33 @@
+﻿using Ecommerce.API.Contracts;
+using Ecommerce.API.Data;
+using Ecommerce.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.API.Repositories;
+
+public class FavoriteRepository : BaseRepository<Favorite>, IFavoriteRepository
+{
+    public FavoriteRepository(EcommerceContext ecommerceContext) : base(ecommerceContext)
+    {
+    }
+
+    public async Task<Favorite?> GetFavoriteById(string favoriteId, bool trackChanges)
+    {
+        return await FindByCondition(f => f.Id == favoriteId, trackChanges)
+            .Include(f => f.ProductItem)
+            .SingleOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<Favorite>> GetFavoritesByUserId(string userId, bool trackChanges)
+    {
+        return await FindByCondition(f => f.UserId == userId, trackChanges)
+            .Include(f => f.ProductItem)
+            .ToListAsync();
+    }
+
+    public void AddFavorite(Favorite favorite) => Create(favorite);
+
+    public void UpdateFavorite(Favorite favorite) => Update(favorite);
+
+    public void DeleteFavorite(Favorite favorite) => Delete(favorite);
+}
