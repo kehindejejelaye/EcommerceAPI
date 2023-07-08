a523c1755e97c4ae49b1b7d76274c741dfb061aa
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-08 22:12:32 +0100

    Added ReviewRepository.cs and its corresponding interface

---
+++ Ecommerce.API/Contracts/IReviewRepository.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.API.Entities;
+
+namespace Ecommerce.API.Contracts
+{
+    public interface IReviewRepository
+    {
+        void CreateReview(Review review);
+        void DeleteReview(Review review);
+        Task<Review?> GetUserReviewForProudctItem(string userId, string productItemId, bool trackChanges);
+        Task<double> GetAverageRatingForProduct(string productItemId);
+        Task<IEnumerable<Review>> GetReviewsForProduct(string productItemId, bool trackChanges);
+        void UpdateReview(Review review);
+    }
+}
