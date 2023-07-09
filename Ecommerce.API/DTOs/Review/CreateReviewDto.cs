4df40a6466cec3add9b2af0b1b0d10736d5c8647
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-09 02:28:00 +0100

    Added range attribute to limit the values that can be received from request payload

--- Ecommerce.API/DTOs/Review/CreateReviewDto.cs
+++ Ecommerce.API/DTOs/Review/CreateReviewDto.cs
@@ -1,4 +1,5 @@
 ﻿using Ecommerce.API.Entities;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Ecommerce.API.DTOs.Review;
@@ -6,6 +7,8 @@ namespace Ecommerce.API.DTOs.Review;
 public class CreateReviewDto
 {
     public string Comment { get; set; }
+
+    [Range(1, 5)]
     public int RatingValue { get; set; }
     public string ProductItemId { get; set; }
     public string UserId { get; set; }
--- Ecommerce.API/DTOs/Review/UpdateReviewDto.cs
+++ Ecommerce.API/DTOs/Review/UpdateReviewDto.cs
@@ -1,8 +1,12 @@
-﻿namespace Ecommerce.API.DTOs.Review;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.API.DTOs.Review;
 
 public class UpdateReviewDto
 {
     public string Comment { get; set; }
+
+    [Range(1, 5)]
     public int RatingValue { get; set; }
     public string ProductItemId { get; set; }
     public string UserId { get; set; }
