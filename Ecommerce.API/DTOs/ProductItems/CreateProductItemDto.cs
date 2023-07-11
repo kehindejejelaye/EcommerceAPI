b4ce3ec8a93ac1e7bd602303b50efbfd50d1e2a3
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2023-07-11 14:53:59 +0100

    Updated ProductItemDtos to include name and description properties

--- Ecommerce.API/DTOs/ProductItems/CreateProductItemDto.cs
+++ Ecommerce.API/DTOs/ProductItems/CreateProductItemDto.cs
@@ -3,6 +3,8 @@
 public class CreateProductItemDto
 {
     public string SKU { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
     public string QuantityInStock { get; set; }
     public string ImageUrl { get; set; } = string.Empty;
     public decimal Price { get; set; }
